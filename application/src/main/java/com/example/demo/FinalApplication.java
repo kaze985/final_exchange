69b0a6b205779912f58b08e36acb6e9523c00b97
kaze8888 <2082856592@qq.com>
2022-07-26 19:45:08 +0800

    init

---
+++ application/src/main/java/com/example/demo/FinalApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = {"com.example.demo","com.geekgame.demo"})
+@MapperScan(basePackages = {"com.geekgame.demo.dao"})
+@EnableDiscoveryClient
+public class FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalApplication.class, args);
+	}
+
+}
